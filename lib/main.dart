b25a73159dc34138d9d19b9200c44c505410396b
moayad-khader <moayadkhader99@gmail.com>
2022-04-23 04:34:13 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -11,60 +11,99 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text(
-          "mkhader app",
+          "mkhader Profile",
           style: TextStyle(
               fontSize: 20,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'IndieFlower'
+              fontWeight: FontWeight.bold
 
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Row(
-        children: <Widget>[
-
-          Expanded(
-            flex: 1,
-              child: Image.network("https://i.ytimg.com/vi/c063ddhWafo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCjDzuM8m7hEdZu8NJyTLZQMztvkA")
-          ),
-
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.cyan[400],
-              child: Text("1"),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/58187169?v=4"),
+                radius: 40.0,
+              ),
             ),
-          ),
-
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.pink[400],
-              child: Text("2"),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
             ),
-          ),
-
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.amber[400],
-              child: Text("3"),
+            Text(
+                "Name",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
             ),
-          ),
-        ],
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.red[600],
-        child: Text("button"),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Moayad Khader",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "CURRENT LEVEL",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "10",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "moayadkhader@outlook.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
